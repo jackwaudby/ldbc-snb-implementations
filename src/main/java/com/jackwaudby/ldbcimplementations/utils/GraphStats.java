c18db69a5c171b931d11cc30481c2fd528f42c86
jackwaudby <j.waudby2@ncl.ac.uk>
2019-10-06 14:45:27 +0100

    testing update 1

--- src/main/java/com/jackwaudby/ldbcimplementations/utils/GraphStats.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/utils/GraphStats.java
@@ -2,6 +2,8 @@ package com.jackwaudby.ldbcimplementations.utils;
 
 import org.apache.log4j.Logger;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
 
 /**
  * This script provides basic graph statistics.
@@ -10,12 +12,24 @@ public class GraphStats {
 
     private static final Logger LOGGER = Logger.getLogger(GraphStats.class);
 
+    public static void main(String[] args) {
+
+        JanusGraph graph = JanusGraphFactory.open("/Users/jackwaudby/janusgraph-0.4.0-hadoop2/conf/janusgraph-berkeleyje.properties");
+        GraphTraversalSource g = graph.traversal(); // create traversal source
+        elementCount(g);
+        CloseGraph.closeGraph(g);
+        graph.close();
+    }
+
     public static void elementCount(GraphTraversalSource g){
 
-        Long vc = g.V().count().next();
-        LOGGER.info("Vertex Count: "+ vc);
-        Long ec = g.E().count().next();
-        LOGGER.info("Edge Count: "+ ec);
+
+//        Long vc = g.V().count().next();
+//        LOGGER.info("Vertex Count: "+ vc);
+        Long persons = g.V().hasLabel("Person").count().next();
+        LOGGER.info("Vertex Count: "+ persons);
+//        Long ec = g.E().count().next();
+//        LOGGER.info("Edge Count: "+ ec);
 
     }
 
