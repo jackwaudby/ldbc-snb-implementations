3f47bbd716bf940e03481ab4d73d2ad6fd5b6c02
jackwaudby <j.waudby2@ncl.ac.uk>
2019-10-09 18:06:37 +0100

    implementation of short read query 3

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery3PersonFriendsHandler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery3PersonFriendsHandler.java
@@ -4,11 +4,14 @@ import com.jackwaudby.ldbcimplementations.JanusGraphDb;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery2PersonPostsResult;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery3PersonFriends;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery3PersonFriendsResult;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultList.httpResponseToResultList;
 import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultMap.httpResponseToResultMap;
 
 public class LdbcShortQuery3PersonFriendsHandler implements OperationHandler<LdbcShortQuery3PersonFriends, JanusGraphDb.JanusGraphConnectionState> {
@@ -22,59 +25,25 @@ public class LdbcShortQuery3PersonFriendsHandler implements OperationHandler<Ldb
         JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();
         // gremlin query string
         String queryString = "{\"gremlin\": \"" +
-                "try {" +
-                "personIds=g.V().has('Person','id'," + personId + ").outE('knows').inV().values('id').toList();[];" +
-                "creationDates=g.V().has('Person','id'," + personId + ").outE('knows').values('creationDate').toList();[];" +
-                "result=['personIds':personIds];[];" +
-                "result['creationDates']=creationDates;[];" +
-                "graph.tx().commit();[];" +
-                "} catch (Exception e) {" +
-                "errorMessage=[e.toString()];[];" +
-                "result=[query_error:errorMessage];[];" +
-                "graph.tx().rollback();[];" +
-                "};" +
-                "result;\"" +
-                "}";
-
-        int TX_ATTEMPTS = 0;
-        int TX_RETRIES = 5;
-        LdbcShortQuery3PersonFriendsResult endResult = null;
-        while (TX_ATTEMPTS < TX_RETRIES) {
-            System.out.println("Attempt " + (TX_ATTEMPTS + 1));
-            String response = client.execute(queryString);                                // get response as string
-            HashMap<String, String> result = httpResponseToResultMap(response);      // convert to result map
-            if (result.containsKey("query_error")) {
-                TX_ATTEMPTS = TX_ATTEMPTS + 1;
-                System.out.println("Query Error: " + result.get("query_error"));
-            } else if (result.containsKey("http_error")) {
-                TX_ATTEMPTS = TX_ATTEMPTS + 1;
-                System.out.println("Gremlin Server Error: " + result.get("http_error"));
-            } else {
-
-                // sorted on server side
-                // get result as hashamp
-                // for each element of hashamp create reuslt
-
-                // create result object
-//                endResult = new LdbcShortQuery3PersonFriendsResult()
-
-//                        result.get("firstName"),
-//                        result.get("lastName"),
-//                        Long.parseLong(result.get("birthday")),
-//                        result.get("locationIP"),
-//                        result.get("browserUsed"),
-//                        Long.parseLong(result.get("cityId")),
-//                        result.get("gender"),
-//                        Long.parseLong(result.get("creationDate")));
-
-                break;
-            }
+                "g.V().has('Person','id'," + personId + ")" +
+                ".bothE('knows').order().by('creationDate',decr).as('edge').otherV().as('friend').select('edge','friend').by(valueMap('creationDate')).by(valueMap('id','firstName','lastName')).toList();" +
+                "\"}";
+        String response = client.execute(queryString);                          // execute query
+        ArrayList<HashMap<String, String>> result                               // parse result
+                = httpResponseToResultList(response);
+        ArrayList<LdbcShortQuery3PersonFriendsResult> endResult                   // init result list
+                        = new ArrayList<>();
+        for (int i = 0; i < result.size(); i++) {
+            LdbcShortQuery3PersonFriendsResult res =
+                    new LdbcShortQuery3PersonFriendsResult(
+                            Long.parseLong(result.get(i).get("friendId")),
+                            result.get(i).get("friendFirstName"),
+                            result.get(i).get("friendLastName"),
+                            Long.parseLong(result.get(i).get("edgeCreationDate"))
+                    );
+            endResult.add(res);
         }
-//        resultReporter.report(0, endResult, operation);
+        resultReporter.report(0, endResult, operation);
     }
 }
 
-// figure out http response
-// Map vs list
-// test query
-// g.V().has('Person','id',1099511628716).as('friend').V(v).addE('knows').property('creationDate',newdate).next()
