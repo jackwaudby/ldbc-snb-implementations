f74d47c54abdb23c2545f9cb07dbb0e10839c1b4
jackwaudby <j.waudby2@ncl.ac.uk>
2019-10-09 17:55:38 +0100

    refactored parser to handle calling valueMap() on edge element returns a property NOT in a list whereas a vertex element DOES return property values in a list. Also, each property key is appended with path element key to break ties

--- src/main/java/com/jackwaudby/ldbcimplementations/utils/HttpResponseToResultList.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/utils/HttpResponseToResultList.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.json.Json;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,34 +23,45 @@ public class HttpResponseToResultList {
 
     public static ArrayList<HashMap<String, String>> httpResponseToResultList(String httpResponse) {
 
-        JSONObject responseJson = new JSONObject(httpResponse);                             // convert to JSON
+        JSONObject responseJson = new JSONObject(httpResponse);                         // convert to JSON
         JSONArray results = responseJson.getJSONObject("result")                        // get results
                 .getJSONObject("data").getJSONArray("@value");
         ArrayList<HashMap<String, String>> resultList = new ArrayList<>();              // init result arraylist
-        for (int i = 0; i < results.length(); i++) {                                     // for each result
+        for (int i = 0; i < results.length(); i++) {                                    // for each result
             JSONArray result = results.getJSONObject(i).getJSONArray("@value");
             HashMap<String, String> propertyPair = new HashMap<>();                     // init hashmap
-            for (int j = 0; j < result.length(); j++) {                                  // for each path element
+            for (int j = 0; j < result.length(); j++) {                                 // for each path element
                 String pathElementKey = result.get(j).toString();                       // get path element key
                 j = j + 1;
                 JSONArray pathElementValue =
                         result.getJSONObject(j).getJSONArray("@value");             // get path element value
-                for (int k = 0; k < pathElementValue.length(); k++) {                     // for each property in path element value
-                    String propertyKey = pathElementValue.get(k).toString();             // get property key
-                    if (propertyKey.equals("id")) {                                      // multiple path element can have same property key - break on path element name
-                        propertyKey = pathElementKey +
-                                propertyKey.substring(0, 1).toUpperCase() + propertyKey.substring(1);
+                try {                                                                    // if path element is a vertex
+                    for (int k = 0; k < pathElementValue.length(); k++) {                // for each property in path element value
+                        String propertyKey = pathElementValue.get(k).toString();         // get property key
+                        propertyKey = pathElementKey +                                       // append element name to property key
+                                propertyKey.substring(0, 1).toUpperCase() +
+                                propertyKey.substring(1);
+                        k = k + 1;
+                        String propertyValue;
+                        try {                                                                // get property value, either String or JSONObject (converted to String)
+                            propertyValue = pathElementValue.getJSONObject(k)
+                                    .getJSONArray("@value").getJSONObject(0).get("@value").toString();
+                        } catch (JSONException e) {
+                            propertyValue = pathElementValue
+                                    .getJSONObject(k).getJSONArray("@value").getString(0);
+                        }
+                        propertyPair.put(propertyKey, propertyValue);                        // put in hashmap
                     }
-                    k = k + 1;
-                    String propertyValue;
-                    try {                                                                // get property value, either String or JSONObject (converted to String)
-                        propertyValue = pathElementValue.getJSONObject(k)
-                                .getJSONArray("@value").getJSONObject(0).get("@value").toString();
-                    } catch (JSONException e) {
-                        propertyValue = pathElementValue
-                                .getJSONObject(k).getJSONArray("@value").getString(0);
+                } catch (JSONException e ){                                         // if path element is an edge
+                    for (int k = 0; k < pathElementValue.length(); k++) {                    // for each property in path element value
+                        String propertyKey = pathElementValue.get(k).toString();             // get property key
+                        propertyKey = pathElementKey +                                       // append element name to property key
+                                propertyKey.substring(0, 1).toUpperCase() +
+                                propertyKey.substring(1);                                    // multiple path element can have same property key - break on path element name
+                        k = k + 1;
+                        String propertyValue = pathElementValue.getJSONObject(k).get("@value").toString();
+                        propertyPair.put(propertyKey, propertyValue);                        // put in hashmap
                     }
-                    propertyPair.put(propertyKey, propertyValue);                        // put in hashmap
                 }
             }
             resultList.add(propertyPair);                                               // add hashmap for result in arraylist
