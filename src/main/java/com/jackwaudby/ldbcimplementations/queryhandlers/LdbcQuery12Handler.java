cbe180e5cad5866141b7a3508c1a27f03a32e9ef
jackwaudby <j.waudby2@ncl.ac.uk>
2019-10-26 21:00:30 +0100

    query 12 class file

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcQuery12Handler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcQuery12Handler.java
@@ -10,7 +10,6 @@ package com.jackwaudby.ldbcimplementations.queryhandlers;
  *  but only collect Tags with the given TagClass or with a descendant of that TagClass
  * Return Persons with at least one reply, the reply count, and the collection of Tags.
  */
-// g.V().sideEffect(hasLabel('Tag').where(out('hasType').has('TagClass','name','GolfPlayer')).aggregate('cond')).has('Person','id',3298534885557).as('person').both('knows').as('friend').where(__.in('hasCreator').hasLabel('Comment').out('replyOf').hasLabel('Post').out('hasTag').where(within('cond'))).as('person').select('person').by(__.in('hasCreator').hasLabel('Comment').out('replyOf').hasLabel('Post').out('hasTag').where(within('cond')).dedup().values('name').fold()).as('tagNames').select('person').by(__.in('hasCreator').hasLabel('Comment').out('replyOf').hasLabel('Post').out('hasTag').where(within('cond')).values('name').count().fold()).as('replyCount').select('person','tagNames','replyCount').by(valueMap('id','firstName','lastName')).by().by()
 
 public class LdbcQuery12Handler {
 
