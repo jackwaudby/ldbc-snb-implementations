a025bf87128f19fb5b44f8d07c38fd6de7123009
jackwaudby <j.waudby2@ncl.ac.uk>
2019-10-26 02:15:53 +0100

    testing query 3

--- src/main/java/com/jackwaudby/ldbcimplementations/QueryTestBed.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/QueryTestBed.java
@@ -1,5 +1,6 @@
 package com.jackwaudby.ldbcimplementations;
 
+import com.sun.xml.internal.ws.protocol.soap.ClientMUTube;
 import org.json.JSONObject;
 import java.util.*;
 
@@ -11,14 +12,33 @@ import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.*;
 public class QueryTestBed {
     public static void main(String[] args) {
 
+        // ["com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery3",5497558139286,"Lithuania","Estonia",1283299200000,41,20]|
+// [[2194,"Alexander","Dobrunov",2,1,3],
+// [1490,"John","Wilson",1,1,2],
+// [1099511628352,"Joseph","Linchuk",1,1,2],
+// [3298534883809,"Antonio","Alvarez",1,1,2]]
+
+        long personId = 5497558139286L;
+        String countryX = "Lithuania";
+        String countryY = "Estonia";
+        long startDate = 1283299200000L;
+        long duration = 41L;
+        long endDate = startDate + (duration * 86400000L);
+        long limit = 20;
+
+
         String queryString = "{\"gremlin\": \"" +
-                " g.V().has('Person','id',4398046514041)." +
-                " choose(" +
-                " repeat(both().dedup()).until(has('Person','id',3)).limit(1).path().count(local).is(gt(0))," +
-                " repeat(both().simplePath()).until(has('Person','id',3)).limit(1).path().count(local)." +
-                " constant(-1)" +
-                " )." +
-                " fold()" +
+                "g.V().has('Person','id',"+personId+").repeat(both('knows').simplePath()).emit().times(2).dedup()." +
+                "where(and(out('isLocatedIn').out('isPartOf').has('name',without('"+countryX+"','"+countryY+"'))," +
+                "local(__.in('hasCreator').has('creationDate',between(new Date("+startDate+"),new Date("+endDate+")))." +
+                "out('isLocatedIn').has('name','"+countryX+"').count().is(gt(0)))," +
+                "local(__.in('hasCreator').has('creationDate',between(new Date("+startDate+"),new Date("+endDate+")))." +
+                "out('isLocatedIn').has('name','"+countryY+"').count().is(gt(0)))))." +
+                "order().by(__.in('hasCreator').has('creationDate',between(new Date("+startDate+"),new Date("+endDate+")))." +
+                "out('isLocatedIn').has('name','"+countryX+"').count(),desc).by('id',asc)." +
+                "local(union(identity().valueMap('id','firstName','lastName').by(unfold()).unfold(),__.in('hasCreator')." +
+                "has('creationDate',between(new Date("+startDate+"),new Date("+endDate+")))."+
+                "out('isLocatedIn').has('name',within('"+countryX+"','"+countryY+"')).group().by('name').by(count()).unfold()).fold())" +
                 "\"" +
                 "}";
 
@@ -27,9 +47,8 @@ public class QueryTestBed {
         JanusGraphDb x = new JanusGraphDb();
         x.init(hm);
         String response = x.execute(queryString);
-
-        ArrayList<JSONObject> resultList = gremlinResponseToResultArrayList(response);
-        System.out.println(resultList);
+        ArrayList<JSONObject> results = gremlinResponseToResultArrayList(response);
+        System.out.println(results);
 
         x.onClose();
     }
