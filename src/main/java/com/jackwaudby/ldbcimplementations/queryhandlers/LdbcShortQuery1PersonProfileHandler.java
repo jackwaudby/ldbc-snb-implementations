dd1811e9d1c2e00c0685d3d972a3db374420cf67
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-01 00:57:39 +0000

    added transaction handling logic to short read query 1

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery1PersonProfileHandler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery1PersonProfileHandler.java
@@ -1,79 +1,80 @@
 package com.jackwaudby.ldbcimplementations.queryhandlers;
 
 import com.jackwaudby.ldbcimplementations.JanusGraphDb;
+import com.jackwaudby.ldbcimplementations.utils.ImplementationConfiguration;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfile;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfileResult;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultList.httpResponseToResultList;
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultMap.httpResponseToResultMap;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.*;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.gremlinMapToHashMap;
 
 /**
  * Given a start Person, retrieve their first name, last name, birthday, IP
- * address, browser, and city of residence.
+ * address, browser, gender, creation date and the ID of their city of residence.
  */
 public class LdbcShortQuery1PersonProfileHandler implements OperationHandler<LdbcShortQuery1PersonProfile, JanusGraphDb.JanusGraphConnectionState> {
 
-    // TODO: Add transaction logic to query string
     // TODO: Add transaction retry logic to response
 
     @Override
     public void executeOperation(LdbcShortQuery1PersonProfile operation, JanusGraphDb.JanusGraphConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
 
-        // get query parameter from operation
-        long personId = operation.personId();
-        // get JanusGraph client
-        JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();
-        // gremlin query string
+        long personId = operation.personId();                                   // get query parameter from operation
+        JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();   // get JanusGraph client
         String queryString = "{\"gremlin\": \"" +                               // gremlin query string
-                "g.V().has('Person','id'," + personId + ").as('person').out('isLocatedIn').as('city').select('person','city').by(valueMap('firstName','lastName','birthday','locationIP','browserUsed','gender','creationDate')).by(valueMap('id')).toList();" +
+                "graph.tx().rollback();[];" +
+                "try{" +
+                  "result = g.V().has('Person','id',"+personId+")." +
+                             "union(" +
+                               "valueMap('firstName','lastName','birthday','locationIP','browserUsed','gender','creationDate').unfold(),"+
+                               "out('isLocatedIn').valueMap('id').unfold()" +
+                             ").fold().toList();" +
+                  "graph.tx().commit();[];"+
+                "} catch (Exception e) {"+
+                  "errorMessage =[e.toString()];[];" +
+                  "result=[error:errorMessage];" +
+                  "graph.tx().rollback();[];" +
+                "};" +
+                "result" +
                 "\"" +
                 "}";
 
-        String response = client.execute(queryString);                          // execute query
-        ArrayList<HashMap<String, String>> result                               // parse result
-                = httpResponseToResultList(response);
-        ArrayList<LdbcShortQuery1PersonProfileResult> endResult                   // init result list
-                = new ArrayList<>();
-        for (int i = 0; i < result.size(); i++) {                               // for each result
-            LdbcShortQuery1PersonProfileResult res                                    // create result object
-                    = new LdbcShortQuery1PersonProfileResult(
-                            result.get(i).get("personFirstName"),
-                        result.get(i).get("personLastName"),
-                        Long.parseLong(result.get(i).get("personBirthday")),
-                        result.get(i).get("personLocationIP"),
-                        result.get(i).get("personBrowserUsed"),
-                        Long.parseLong(result.get(i).get("cityId")),
-                        result.get(i).get("personGender"),
-                        Long.parseLong(result.get(i).get("personCreationDate"))
-            );
-            endResult.add(res);                                                 // add to result list
-        }
+        int TX_ATTEMPTS = 0;                                                                 // init. transaction attempts
+        int TX_RETRIES = new ImplementationConfiguration().getTxnAttempts();
+        String response = client.execute(queryString);                                       // execute query
+        ArrayList<JSONObject> results = gremlinResponseToResultArrayList(response);          // get result list
+        ArrayList<JSONObject> result = gremlinListToArrayList(results.get(0));               // get result
+        LdbcShortQuery1PersonProfileResult ldbcShortQuery1PersonProfileResult                // create result object
+                = new LdbcShortQuery1PersonProfileResult(
+                getPropertyValue(gremlinMapToHashMap(result.get(3)).get("firstName")),
+                getPropertyValue(gremlinMapToHashMap(result.get(4)).get("lastName")),
+                Long.parseLong(getPropertyValue(gremlinMapToHashMap(result.get(6)).get("birthday"))),
+                getPropertyValue(gremlinMapToHashMap(result.get(2)).get("locationIP")),
+                getPropertyValue(gremlinMapToHashMap(result.get(1)).get("browserUsed")),
+                Long.parseLong(getPropertyValue(gremlinMapToHashMap(result.get(7)).get("id"))),
+                getPropertyValue(gremlinMapToHashMap(result.get(5)).get("gender")),
+                Long.parseLong(getPropertyValue(gremlinMapToHashMap(result.get(0)).get("creationDate")))
+        );
 
-//        String queryString = "{\"gremlin\": \"" +
-//                "try {" +
-//                "v = g.V().has('Person','id'," + personId + ").next();[];" +
-//                "hm = g.V(v).valueMap('firstName','lastName','birthday','locationIP','browserUsed','gender','creationDate').next();[];" +
-//                "v2 = g.V(v).outE('isLocatedIn').inV().valueMap('id').next();[];" +
-//                "cityId = v2['id'];[];" +
-//                "hm.put('cityId',cityId);[];" +
-//                "graph.tx().commit();[];" +
-//                "} catch (Exception e) {" +
-//                "errorMessage =[e.toString()];[];" +
-//                "hm=[query_error:errorMessage];[];" +
-//                "graph.tx().rollback();[];" +
-//                "};" +
-//                "hm;\"" +
-//                "}";
+        resultReporter.report(0, ldbcShortQuery1PersonProfileResult, operation); // pass to driver
+
+    }
+}
 
 
-//        int TX_ATTEMPTS = 0;
-//        int TX_RETRIES = 5;
+
+
+
+
+
+
+
 //        LdbcShortQuery1PersonProfileResult endResult = null;
 //        while (TX_ATTEMPTS < TX_RETRIES) {
 //            System.out.println("Attempt " + (TX_ATTEMPTS + 1));
@@ -100,6 +101,4 @@ public class LdbcShortQuery1PersonProfileHandler implements OperationHandler<Ldb
 //                break;
 //            }
 //        }
-        resultReporter.report(0, endResult.get(0), operation);
-    }
-}
+
