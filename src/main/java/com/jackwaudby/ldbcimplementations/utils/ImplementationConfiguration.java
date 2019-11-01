e75988bec8bbdd554f5a6a20815991d7aa119b87
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-01 01:51:10 +0000

    added some documentation

--- src/main/java/com/jackwaudby/ldbcimplementations/utils/ImplementationConfiguration.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/utils/ImplementationConfiguration.java
@@ -9,17 +9,24 @@ import java.util.Properties;
  */
 public class ImplementationConfiguration {
 
-    private String configurationPath = "implementation-configuration.properties";
     private Properties implementationProperties = new Properties();
 
+    /**
+     * Load in properties file.
+     */
     public ImplementationConfiguration() {
         try {
+            String configurationPath = "implementation-configuration.properties";
             implementationProperties.load(new FileInputStream(configurationPath));
         } catch (IOException e) {
             e.printStackTrace(); // TODO: Add error to logger
         }
     }
 
+    /**
+     * Get transaction retry attempts.
+     * @return transaction attempts
+     */
     public Integer getTxnAttempts() {
         return Integer.parseInt(implementationProperties.getProperty("txn.attempts"));
     }
