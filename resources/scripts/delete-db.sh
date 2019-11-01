0cfdc58c74383c3d2ec32c1251ad353144a9e9f7
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-01 02:03:22 +0000

    clean up

--- resources/scripts/delete-db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# if running gremlin server, stop server
-
-# remove db directory
-rm -Rf ${JANUSGRAPH_HOME}/db/berkeley/
--- resources/scripts/start-gremlin-server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-$JANUSGRAPH_HOME/bin/gremlin-server.sh  $JANUSGRAPH_HOME/conf/gremlin-server/gremlin-server-berkeleyje.yaml
