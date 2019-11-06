7f0146e4c804f7ba6dc47ca95b8a787caaa72259
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-06 16:53:11 +0000

    refactored short read 3, added transaction logic

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery3PersonFriendsHandler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery3PersonFriendsHandler.java
@@ -4,47 +4,82 @@ import com.jackwaudby.ldbcimplementations.JanusGraphDb;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery2PersonPostsResult;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery3PersonFriends;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery3PersonFriendsResult;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultList.httpResponseToResultList;
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultMap.httpResponseToResultMap;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.*;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.gremlinMapToHashMap;
+import static com.jackwaudby.ldbcimplementations.utils.ImplementationConfiguration.getTxnAttempts;
 
+/**
+ * Given a start Person, retrieve all of their friend's ID, firstName, lastName, and the date at which they became friends.
+ */
 public class LdbcShortQuery3PersonFriendsHandler implements OperationHandler<LdbcShortQuery3PersonFriends, JanusGraphDb.JanusGraphConnectionState> {
+
+    private static Logger LOGGER = Logger.getLogger(LdbcShortQuery3PersonFriendsHandler.class.getName());
+
 
     @Override
     public void executeOperation(LdbcShortQuery3PersonFriends operation, JanusGraphDb.JanusGraphConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
 
-        // get query parameter from operation
-        long personId = operation.personId();
-        // get JanusGraph client
-        JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();
-        // gremlin query string
+        long personId = operation.personId();                                   // start person id
+        JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();   // janusgraph client
+
         String queryString = "{\"gremlin\": \"" +
-//                "g.V().has('Person','id'," + personId + ")" +
-//                ".bothE('knows').order().by('creationDate',decr).as('edge').otherV().as('friend').select('edge','friend').by(valueMap('creationDate')).by(valueMap('id','firstName','lastName')).toList();" +
-                "g.V().has('Person','id'," + personId + ").bothE('knows').as('edge').otherV().as('friend').order().by(select('edge').by('creationDate'),desc).by('id',asc).select('edge','friend').by(valueMap('creationDate')).by(valueMap('id','firstName','lastName')).toList();" +
-                "\"}";
-        String response = client.execute(queryString);                          // execute query
-        ArrayList<HashMap<String, String>> result                               // parse result
-                = httpResponseToResultList(response);
-        ArrayList<LdbcShortQuery3PersonFriendsResult> endResult                   // init result list
+                "graph.tx().rollback();[];" +
+                "try{" +
+                "result = g.V().has('Person','id'," + personId + ").bothE('knows').as('friendshipCreationDate').otherV().as('friend')." +
+                  "order().by(select('friendshipCreationDate').by('creationDate'),desc)." +
+                    "by('id',asc).select('friendshipCreationDate','friend')." +
+                    "by(values('creationDate').fold()).by(valueMap('id','firstName','lastName')).toList();" +
+                "graph.tx().commit();[];"+
+                "} catch (Exception e) {"+
+                "errorMessage =[e.toString()];[];" +
+                "result=[error:errorMessage];" +
+                "graph.tx().rollback();[];" +
+                "};" +
+                "result" +
+                "\"" +
+                "}";
+
+        int TX_ATTEMPTS = 0;                                                                 // init. transaction attempts
+        int TX_RETRIES = getTxnAttempts();
+
+        while (TX_ATTEMPTS < TX_RETRIES) {
+            LOGGER.info("Attempt " + (TX_ATTEMPTS + 1) + ": " + LdbcShortQuery3PersonFriendsHandler.class.getSimpleName());
+            String response = client.execute(queryString);                                            // execute query
+            ArrayList<JSONObject> results = gremlinResponseToResultArrayList(response);          // get result list
+            if (gremlinMapToHashMap(results.get(0)).containsKey("error")) {
+                LOGGER.error(getPropertyValue(gremlinMapToHashMap(results.get(0)).get("error")));
+                TX_ATTEMPTS = TX_ATTEMPTS + 1;
+            } else {
+                ArrayList<LdbcShortQuery3PersonFriendsResult> queryResultList                   // init result list
                         = new ArrayList<>();
-        for (int i = 0; i < result.size(); i++) {
-            LdbcShortQuery3PersonFriendsResult res =
-                    new LdbcShortQuery3PersonFriendsResult(
-                            Long.parseLong(result.get(i).get("friendId")),
-                            result.get(i).get("friendFirstName"),
-                            result.get(i).get("friendLastName"),
-                            Long.parseLong(result.get(i).get("edgeCreationDate"))
-                    );
-            endResult.add(res);
+                for (JSONObject result : results
+                ) {
+                    long friendshipCreationDate = Long.parseLong(getPropertyValue(gremlinMapToHashMap(result).get("friendshipCreationDate")));
+
+                    JSONObject friend = gremlinMapToHashMap(result).get("friend");
+                    long originalPostId = Long.parseLong(getPropertyValue(gremlinMapToHashMap(friend).get("id")));
+                    String originalAuthorFirstName = getPropertyValue(gremlinMapToHashMap(friend).get("firstName"));
+                    String originalAuthorLastName = getPropertyValue(gremlinMapToHashMap(friend).get("lastName"));
+
+                    LdbcShortQuery3PersonFriendsResult ldbcShortQuery3PersonFriendsResult =
+                            new LdbcShortQuery3PersonFriendsResult(
+                                    originalPostId,
+                                    originalAuthorFirstName,
+                                    originalAuthorLastName,
+                                    friendshipCreationDate);
+                    queryResultList.add(ldbcShortQuery3PersonFriendsResult);
+                }
+                resultReporter.report(0, queryResultList, operation);              // pass to result reporter
+                break;
+            }
         }
-        resultReporter.report(0, endResult, operation);
     }
 }
 
