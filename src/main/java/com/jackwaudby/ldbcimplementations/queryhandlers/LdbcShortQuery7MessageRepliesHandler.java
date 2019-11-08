38074268eb6270337d3c6e8d6702d74a6068323e
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-08 18:32:08 +0000

    added transaction logic

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery7MessageRepliesHandler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery7MessageRepliesHandler.java
@@ -1,66 +1,104 @@
 package com.jackwaudby.ldbcimplementations.queryhandlers;
 
 import com.jackwaudby.ldbcimplementations.JanusGraphDb;
+import com.jackwaudby.ldbcimplementations.QueryTestBed;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery7MessageReplies;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery7MessageRepliesResult;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultList.httpResponseToResultList;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.*;
+import static com.jackwaudby.ldbcimplementations.utils.ImplementationConfiguration.getTxnAttempts;
 
+/**
+ * Given a Message, retrieve the (1-hop) Comments that reply to it.
+ * In addition, return a boolean flag knows indicating if the author of the reply knows the author of the original message.
+ * If author is same as original author, return false for knows flag.
+ */
 public class LdbcShortQuery7MessageRepliesHandler implements OperationHandler<LdbcShortQuery7MessageReplies, JanusGraphDb.JanusGraphConnectionState> {
+
+    private static Logger LOGGER = Logger.getLogger(LdbcShortQuery7MessageRepliesHandler.class.getName());
+
 
     @Override
     public void executeOperation(LdbcShortQuery7MessageReplies operation, JanusGraphDb.JanusGraphConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
 
-        // originalMessage=g.V().has('Post','id',34359748472).fold().coalesce(unfold(),V().has('Comment','id',34359748472)).next()
-        // originalAuthor=g.V(originalMessage).out('hasCreator').next()
-        // replies=g.V(originalMessage).in('replyOf').order().by('creationDate',desc).by('id',asc).toList()
-        // repliesAuthor=g.V(replies).out('hasCreator').as('replyAuthor').toList()
-        // g.V(repliesAuthor).as('repliesAuthor').choose(bothE('knows').otherV().hasId(originalAuthor.id()),constant(true),constant(false)).as('knows').select('knows','repliesAuthor')
-        // g.V().has('Post','id',34359748472).fold().coalesce(unfold(),V().has('Comment','id',34359748472)).as('originalMessage').in('replyOf').as('comment').order().by(select('comment').by('creationDate'),desc).by('id',asc).out('hasCreator').as('replyAuthor').choose(bothE('knows').otherV().hasId(originalAuthor.id()),constant(true),constant(false)).as('knows').select('comment','replyAuthor','knows').by(valueMap('id','content','creationDate')).by(valueMap('id','firstName','lastName')).by()
-
-
- // TODO: Add transaction logic to query string
- // TODO: Add transaction retry logic to response
-
- long messageId = operation.messageId();
-    JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();   // janusgraph client
-//    String queryString = "{\"gremlin\": \"" +                               // gremlin query string
-//            "originalMessage=g.V().has('Post','id',"+messageId+").fold().coalesce(unfold(),V().has('Comment','id',"+messageId+")).next();" +
-//            "originalAuthor=g.V(originalMessage).out('hasCreator').next();" +
-//            "g.V(originalMessage).in('replyOf').as('comment').order().by(select('comment').by('creationDate'),desc).by('id',asc).out('hasCreator').as('replyAuthor').select('comment','replyAuthor').by(valueMap('id','content','creationDate')).by(valueMap('id','firstName','lastName'))" +
-//            "\"" +
-//            "}";
+        long messageId = operation.messageId();
+        JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();   // janusgraph client
         String queryString = "{\"gremlin\": \"" +                               // gremlin query string
-                "originalMessage=g.V().has('Post','id',"+messageId+").fold().coalesce(unfold(),V().has('Comment','id',"+messageId+")).next();" +
-                "originalAuthor=g.V(originalMessage).out('hasCreator').next();" +
-                "g.V(originalMessage).as('originalMessage').in('replyOf').as('comment').order().by(select('comment').by('creationDate'),desc).by('id',asc).out('hasCreator').as('replyAuthor').choose(bothE('knows').otherV().hasId(originalAuthor.id()),constant(true),constant(false)).as('knows').select('comment','replyAuthor','knows').by(valueMap('id','content','creationDate')).by(valueMap('id','firstName','lastName')).by(fold());" +
+                "graph.tx().rollback();[];" +
+                "try{" +
+                "originalMessage=g.V().has('Post','id',"+messageId+").fold().coalesce(unfold(),V().has('Comment','id',"+messageId+")).next();[];" +
+                "originalAuthor=g.V(originalMessage).out('hasCreator').next();[];" +
+                "result=g.V(originalMessage).as('originalMessage').in('replyOf').as('comment')." +
+                "order().by(select('comment').by('creationDate'),desc).by(select('comment').by('id'),asc)." +
+                "out('hasCreator').as('replyAuthor')." +
+                "choose(bothE('knows').otherV().hasId(originalAuthor.id()),constant(true),constant(false)).as('knows')." +
+                "select('comment','replyAuthor','knows')." +
+                "by(valueMap('id','content','creationDate')).by(valueMap('id','firstName','lastName')).by(fold())." +
+                "map{it -> [" +
+                "commentId:it.get().get('comment').get('id')," +
+                "commentContent:it.get().get('comment').get('content')," +
+                "commentCreationDate:it.get().get('comment').get('creationDate')," +
+                "replyAuthorId:it.get().get('replyAuthor').get('id')," +
+                "replyAuthorFirstName:it.get().get('replyAuthor').get('firstName')," +
+                "replyAuthorLastName:it.get().get('replyAuthor').get('lastName')," +
+                "replyAuthorKnowsOriginalMessageAuthor:it.get().get('knows')" +
+                "]};[];" +
+                "graph.tx().commit();[];"+
+                "} catch (Exception e) {"+
+                "errorMessage =[e.toString()];[];" +
+                "result=[error:errorMessage];" +
+                "graph.tx().rollback();[];" +
+                "};" +
+                "result" +
                 "\"" +
                 "}";
-    String response = client.execute(queryString);                          // execute query
-    ArrayList<HashMap<String, String>> result                               // parse result
-            = httpResponseToResultList(response);
-    ArrayList<LdbcShortQuery7MessageRepliesResult> endResult                   // init result list
-                = new ArrayList<>();
-    for (int i = 0; i < result.size(); i++) {                               // for each result
-            LdbcShortQuery7MessageRepliesResult res                                    // create result object
-                    = new LdbcShortQuery7MessageRepliesResult(
-                    Long.parseLong(result.get(i).get("commentId")),
-                    result.get(i).get("commentContent"),
-                    Long.parseLong(result.get(i).get("commentCreationDate")),
-                    Long.parseLong(result.get(i).get("replyAuthorId")),
-                    result.get(i).get("replyAuthorFirstName"),
-                    result.get(i).get("replyAuthorLastName"),
-                    Boolean.parseBoolean(result.get(i).get("knows"))
-            );
-            endResult.add(res);                                                 // add to result list
+
+        int TX_ATTEMPTS = 0;                                                                 // init. transaction attempts
+        int TX_RETRIES = getTxnAttempts();
+
+        while (TX_ATTEMPTS < TX_RETRIES) {
+            LOGGER.info("Attempt " + (TX_ATTEMPTS + 1) + ": " + QueryTestBed.class.getSimpleName());
+            String response = client.execute(queryString);                                            // execute query
+            ArrayList<JSONObject> results = gremlinResponseToResultArrayList(response);          // get result list
+            if (gremlinMapToHashMap(results.get(0)).containsKey("error")) {
+                LOGGER.error(getPropertyValue(gremlinMapToHashMap(results.get(0)).get("error")));
+                TX_ATTEMPTS = TX_ATTEMPTS + 1;
+            } else {
+                ArrayList<LdbcShortQuery7MessageRepliesResult> queryResults                   // init result list
+                        = new ArrayList<>();
+                for (JSONObject result: results
+                ) {
+                    long commentId = Long.parseLong(getPropertyValue(gremlinMapToHashMap(result).get("commentId")));
+                    String commentContent = getPropertyValue(gremlinMapToHashMap(result).get("commentContent"));
+                    long commentCreationDate = Long.parseLong(getPropertyValue(gremlinMapToHashMap(result).get("commentCreationDate")));
+                    long replyAuthorId = Long.parseLong(getPropertyValue(gremlinMapToHashMap(result).get("replyAuthorId")));
+                    String replyAuthorFirstName = getPropertyValue(gremlinMapToHashMap(result).get("replyAuthorFirstName"));
+                    String replyAuthorLastName = getPropertyValue(gremlinMapToHashMap(result).get("replyAuthorLastName"));
+                    boolean replyAuthorKnowsOriginalMessageAuthor = gremlinMapToHashMap(result).get("replyAuthorKnowsOriginalMessageAuthor").getJSONArray("@value").getBoolean(0);
+                    LdbcShortQuery7MessageRepliesResult queryResult                                    // create result object
+                            = new LdbcShortQuery7MessageRepliesResult(
+                            commentId,
+                            commentContent,
+                            commentCreationDate,
+                            replyAuthorId,
+                            replyAuthorFirstName,
+                            replyAuthorLastName,
+                            replyAuthorKnowsOriginalMessageAuthor
+                    );
+                    queryResults.add(queryResult);
+                }
+                resultReporter.report(0, queryResults, operation);
+
+                break;
+            }
         }
-        resultReporter.report(0, endResult, operation);
     }
 }
 
