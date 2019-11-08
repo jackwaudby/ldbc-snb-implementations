6b97917eb90215cc593dd1acc4d422cacfe70dcf
jackwaudby <j.waudby2@ncl.ac.uk>
2019-11-08 14:49:43 +0000

    added transaction logic

--- src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery5MessageCreatorHandler.java
+++ src/main/java/com/jackwaudby/ldbcimplementations/queryhandlers/LdbcShortQuery5MessageCreatorHandler.java
@@ -4,67 +4,72 @@ import com.jackwaudby.ldbcimplementations.JanusGraphDb;
 import com.ldbc.driver.DbException;
 import com.ldbc.driver.OperationHandler;
 import com.ldbc.driver.ResultReporter;
-import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery1PersonProfileResult;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery5MessageCreator;
 import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcShortQuery5MessageCreatorResult;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
-import static com.jackwaudby.ldbcimplementations.utils.HttpResponseToResultMap.httpResponseToResultMap;
+import static com.jackwaudby.ldbcimplementations.utils.GremlinResponseParsers.*;
+import static com.jackwaudby.ldbcimplementations.utils.ImplementationConfiguration.getTxnAttempts;
 
 /**
  * Given a Message, retrieve its author and their ID, firstName and lastName.
  */
 public class LdbcShortQuery5MessageCreatorHandler implements OperationHandler<LdbcShortQuery5MessageCreator, JanusGraphDb.JanusGraphConnectionState> {
 
+    private static Logger LOGGER = Logger.getLogger(LdbcShortQuery5MessageCreatorHandler.class.getName());
+
     @Override
     public void executeOperation(LdbcShortQuery5MessageCreator operation, JanusGraphDb.JanusGraphConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
 
-        long messageId = operation.messageId();                                     // query parameters
+        long messageId = operation.messageId();                                     // message id
         JanusGraphDb.JanusGraphClient client = dbConnectionState.getClient();       // get JanusGraph client
-        String queryString = "{\"gremlin\": \"" +                                   // gremlin query string
-                "try {" +
-                "v=g.V().has('Comment','id',"+messageId+").fold()" +
+        String queryString = "{\"gremlin\": \"" +
+                "graph.tx().rollback();[];" +
+                "try{" +
+                "result=g.V().has('Comment','id',"+messageId+").fold()" +
                 ".coalesce(unfold(),V().has('Post','id',"+messageId+"))" +
-                ".out('hasCreator').valueMap('id','firstName','lastName').next();[];" +
-//                "post_exists = g.V().has('Post','id','" + messageId + "').hasNext();[];" +
-//                "if (post_exists) {" +
-//                "v=g.V().has('Post','id','" + messageId + "').out('hasCreator').valueMap('id','firstName','lastName').next();[];" +
-//                "} else {" +
-//                "v=g.V().has('Comment','id','" + messageId + "').out('hasCreator').valueMap('id','firstName','lastName').next();[];" +
-//                "};" +
-                "graph.tx().commit();[];" +
-                "} catch (Exception e) {" +
+                ".out('hasCreator').valueMap('id','firstName','lastName').toList();[];" +
+                "graph.tx().commit();[];"+
+                "} catch (Exception e) {"+
                 "errorMessage =[e.toString()];[];" +
-                "v=[query_error:errorMessage];[];" +
+                "result=[error:errorMessage];" +
                 "graph.tx().rollback();[];" +
                 "};" +
-                "v;\"" +
+                "result" +
+                "\"" +
                 "}";
 
         int TX_ATTEMPTS = 0;
-        int TX_RETRIES = 5;
-        LdbcShortQuery5MessageCreatorResult endResult = null;
+        int TX_RETRIES = getTxnAttempts();
+
         while (TX_ATTEMPTS < TX_RETRIES) {
-            System.out.println("Attempt " + (TX_ATTEMPTS + 1));
-            String response = client.execute(queryString);                           // get http response
-            HashMap<String, String> result = httpResponseToResultMap(response);      // convert to result to map
-            if (result.containsKey("query_error")) {
+            LOGGER.info("Attempt " + (TX_ATTEMPTS + 1) + ": " + LdbcShortQuery5MessageCreatorHandler.class.getSimpleName());
+            String response = client.execute(queryString);                                            // execute query
+            ArrayList<JSONObject> results = gremlinResponseToResultArrayList(response);          // get result list
+            if (gremlinMapToHashMap(results.get(0)).containsKey("error")) {
+                LOGGER.error(getPropertyValue(gremlinMapToHashMap(results.get(0)).get("error")));
                 TX_ATTEMPTS = TX_ATTEMPTS + 1;
-                System.out.println("Query Error: " + result.get("query_error"));
-            } else if (result.containsKey("http_error")) {
-                TX_ATTEMPTS = TX_ATTEMPTS + 1;
-                System.out.println("Gremlin Server Error: " + result.get("http_error"));
             } else {
-                endResult = new LdbcShortQuery5MessageCreatorResult(                 // create result object
-                        Long.parseLong(result.get("id")),                      // author ID
-                        result.get("firstName"),                               // author first name
-                        result.get("lastName")                                 // author last name
+
+                long personId = Long.parseLong(getPropertyValue(gremlinMapToHashMap(results.get(0)).get("id")));
+                String firstName = getPropertyValue(gremlinMapToHashMap(results.get(0)).get("firstName"));
+                String lastName = getPropertyValue(gremlinMapToHashMap(results.get(0)).get("lastName"));
+
+                LdbcShortQuery5MessageCreatorResult queryResult = new LdbcShortQuery5MessageCreatorResult(                 // create result object
+                        personId,
+                        firstName,
+                        lastName
                 );
+                resultReporter.report(0, queryResult, operation);
+
                 break;
             }
         }
-        resultReporter.report(0, endResult, operation);
+
+
 
     }
 }
