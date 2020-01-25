5e2992d6fe0cc2ea404c90b0405c6e6791da2049
jackwaudby <j.waudby2@ncl.ac.uk>
2020-01-25 01:00:08 +0000

    adjusted to run from scripts

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp "target/uber-janusgraph-1.0-SNAPSHOT.jar" com.ldbc.driver.Client -P validation/interactive-benchmark.properties
+java -cp "../target/uber-janusgraph-1.0-SNAPSHOT.jar" com.ldbc.driver.Client -P ../validation/interactive-benchmark.properties
